3361b1a742a8e029fdd4b4578656f4ad12805790
ck-shashank <ckshashank8541@gmail.com>
2026-04-13 11:50:00 +0530

    day 9

--- DSA/src/Sorting/ArrangingCoins.java
+++ DSA/src/Sorting/ArrangingCoins.java
@@ -5,8 +5,30 @@ package Sorting;
 public class ArrangingCoins {
 public static void main(String[] args) {
 	int n=98765;
-	long m=(long)n;
-	int row= (int) ((Math.sqrt(8*m+1)-1)/2);
-	System.out.println(row);
+	
+	// with inbuilt function
+//	long m=(long)n;
+//	int row= (int) ((Math.sqrt(8*m+1)-1)/2);
+//	System.out.println(row);
+	
+	//using binary search
+	long low=0;long high=n;
+	long ans=0;
+	while(low<=high) {
+		long k=low +(high-low)/2;  // find mid
+		long m= k*(k+1)/2; // calculate total sum till mid
+		if(n==m) {  //if equal than return 
+			ans=k;
+			break;
+		}
+		else if(m>n) { //if sum> n than number of row will be less
+			high=k-1;
+		}
+		else {
+			ans=k;
+			low=k+1;
+		}
+	}
+	System.out.println((int)ans);
 }
 }
