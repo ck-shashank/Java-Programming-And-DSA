ae4d97438aa5a3ab32e3946224e9ee3d2e17c3fa
ck-shashank <ckshashank8541@gmail.com>
2026-04-15 14:19:23 +0530

    day  10

---
+++ DSA/src/Sorting/MinInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+public class MinInRotatedSortedArray {
+  public static void main(String[] args) {
+	int[] arr= {4,5,6,7,0,1,2};
+	
+	int low=0;
+	int high=arr.length-1;
+	while(low<high) {
+		int mid=low+(high-low)/2;
+		if(arr[mid]>arr[high]) {
+			low=mid+1;
+		}
+		else high=mid;
+	}
+	System.out.println(arr[low]);		
+}
+}
